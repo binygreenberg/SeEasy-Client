114781ea42e354ba15fc78a3b0cce46df379dd95
djblinick <djblinick@gmail.com>
2016-06-02 10:09:18 +0300

    new tab uses same graph as opening tab

--- client/background.js
+++ client/background.js
@@ -4,11 +4,17 @@ var activeTab = 0;
 var previousUrls;
 var currentTabTree;
 var dontAddTheseUrl = ["chrome://newtab/","http://localhost:8080/"];
+var isNewTab = false;
+var openedTab = null;
+var tabMap = {};
+
+
 
 var typeEnum = {
 	RECOMMENDED: "recommended",
 	LINK: "link",
-	TYPED: "typed"
+	TYPED: "typed",
+	AUTO_SUBFRAME: "auto_subframe"
 }
 
 function node(name, parent, title, type) {
@@ -18,19 +24,59 @@ function node(name, parent, title, type) {
 	this.title = title;
 }
 
+chrome.tabs.onCreated.addListener(function(tab) {
+	isNewTab = true;
+	openedTab = tab;
+	chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+		storage.tabMap[tab.id] = tab.id;
+		chrome.storage.sync.set({'tabMap': storage.tabMap}, function() {
+	          // Notify that we saved.
+	          console.log('changes saved');
+	    });
+	});		
+});
 
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-	delete previousUrls["p" + tabId.toString()];
+	if (!previousUrls) {
+		chrome.storage.sync.get({'previousUrls': {}}, function (storage) {
+			previousUrls = storage.previousUrls || {};
+			delete previousUrls["p" + tabId.toString()];
+		});
+	} else {
+		delete previousUrls["p" + tabId.toString()];
+	}
 	chrome.storage.sync.remove([String(tabId)], function(storage) {});
-
+	if (Object.keys(previousUrls).length > 30) {
+		chrome.tabs.query({}, function(allTabs) {
+	    	for (var tabId in previousUrls) {
+	    		var tabStillExists = false;
+	    		for (i = 0; i < allTabs.length; i++) {
+	    			if (tabId === ("p" + allTabs[i].id.toString())) {
+	    				tabStillExists = true;
+	    				break;
+	    			}
+	    		}
+	    		if (!tabStillExists) {
+	    			delete previousUrls[tabId];
+	    		}
+	    	}
+	    	chrome.storage.sync.set({'previousUrls': previousUrls}, function() {
+		          // Notify that we saved.
+		          console.log('changes saved');
+		    });
+	    });
+	}
 });
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
 	console.log('tab changed to tab' + String(activeInfo.tabId));
-	chrome.storage.sync.get({[String(activeInfo.tabId)]: [],'previousUrls': {}}, function (storage) {
-		previousUrls = storage.previousUrls || {};
-		currentTabTree = storage[String(activeInfo.tabId)] || [];
-		activeTab = activeInfo.tabId;
+	chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+		var mappedTab = storage.tabMap[activeInfo.tabId]
+		chrome.storage.sync.get({[String(mappedTab)]: [],'previousUrls': {}}, function (storage) {
+			previousUrls = storage.previousUrls || {};
+			currentTabTree = storage[String(mappedTab)] || [];
+			activeTab = mappedTab;
+		});
 	});
 });
 
@@ -44,14 +90,13 @@ function handleStateChange(){
   	}
 }
 
-function addVisitToTree(tabId, changeInfo, tab) {
-	if (currentTabTree && previousUrls) {
-		var lastUrlVisitedOnThisTab = previousUrls["p" + tabId.toString()] || "null";
-		var newVisit = new node(changeInfo.url, lastUrlVisitedOnThisTab, tab.title);
+function addVisitToTree(tabId, changeInfo, tab, tabTree = currentTabTree, lastUrlVisitedOnThisTab = "null") {
+	if (tabTree && previousUrls) {
+		var newVisit = new node(changeInfo.url, lastUrlVisitedOnThisTab, (tab.title || changeInfo.url));
 
 		var arrayContainsOppositeDirection = false;
 		var sitePathAlreadyTraversed = false;
-		currentTabTree.forEach(function (record) {
+		tabTree.forEach(function (record) {
 			if (record.name === newVisit.parent && record.parent === newVisit.name) {
 				arrayContainsOppositeDirection = true;
 			} else if (record.name === newVisit.name && record.parent === newVisit.parent) {
@@ -60,45 +105,77 @@ function addVisitToTree(tabId, changeInfo, tab) {
 		});
 
 		if (!arrayContainsOppositeDirection && !sitePathAlreadyTraversed) {
-	  		currentTabTree.push(newVisit);
+	  		tabTree.push(newVisit);
 	  		//for now ajax to server and log the three most similar domains to console
 	  		//addSimilalURLs_(newVisit.name);
-	  		//console.log('added to list: ' + JSON.stringify(currentTabTree));
+	  		//console.log('added to list: ' + JSON.stringify(tabTree));
 		}
 
 	  	previousUrls["p" + tabId.toString()] = changeInfo.url;
-	    chrome.storage.sync.set({[String(tabId)]: currentTabTree, 'previousUrls': previousUrls}, function() {
-	          // Notify that we saved.
-	          console.log('changes saved' + JSON.stringify(currentTabTree));
-	    });
+	  	chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+		  	var mappedTab = storage.tabMap[tabId];
+		  	console.log('the real tabId is  ' + tabId.toString());
+		    chrome.storage.sync.set({[String(mappedTab)]: tabTree, 'previousUrls': previousUrls}, function() {
+		          // Notify that we saved.
+		          console.log('changes saved for' + mappedTab.toString() + 'are : ' + JSON.stringify(tabTree));
+		    });
+		});
+
 	}
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
-  if (changeInfo.url && checkShouldAddUrl(changeInfo.url)) {
+  if (changeInfo.url && checkShouldAddUrl(changeInfo.url) && !isNewTab) {
   	if (!currentTabTree || !previousUrls || activeTab !== tabId) {
-  		chrome.storage.sync.get({[String(tabId)]: [],'previousUrls': {}}, function (storage) {
-    		previousUrls = storage.previousUrls || {};
-    		currentTabTree = storage[String(tabId)] || [];
-    		activeTab = tabId;
-    		addVisitToTree(tabId, changeInfo, tab);
-    	});
+  		chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+	  		var mappedTab = storage.tabMap[tabId];
+			console.log("This is the tabMap in onUpdated : " + JSON.stringify(tabMap));
+			console.log('this is the tabID that I am putting into the map: ' + tabId.toString())
+	  		chrome.storage.sync.get({[String(mappedTab)]: [],'previousUrls': {}}, function (storage) {
+	    		previousUrls = storage.previousUrls || {};
+	    		currentTabTree = storage[String(mappedTab)] || [];
+	    		console.log("Had to reload the data from scratch and it is: " + JSON.stringify(currentTabTree));
+	    		activeTab = tabId;
+	    		var lastUrlVisitedOnThisTab = previousUrls["p" + tabId.toString()] || "null";
+	    		addVisitToTree(tabId, changeInfo, tab, currentTabTree, lastUrlVisitedOnThisTab);
+	    	});
+	    });
   	} else {
-		addVisitToTree(tabId, changeInfo, tab);
+  		console.log("Data was already loaded and it is:" + JSON.stringify(currentTabTree));
+  		var lastUrlVisitedOnThisTab = previousUrls["p" + tabId.toString()] || "null";
+		addVisitToTree(tabId, changeInfo, tab, currentTabTree, lastUrlVisitedOnThisTab);
   	}
   }
 });
 
+chrome.webNavigation.onCommitted.addListener(function(details) {
+	if (isNewTab && openedTab && (details.transitionType === typeEnum.LINK 
+		|| details.transitionType === typeEnum.AUTO_SUBFRAME)) {
+		console.log("This is the tabMap in onCommitted : " + JSON.stringify(tabMap));
+		console.log("In onCommitted the currentTabTree is:" + JSON.stringify(currentTabTree));
+		chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+			storage.tabMap[openedTab.id] = openedTab.openerTabId;
+			chrome.storage.sync.set({'tabMap': storage.tabMap}, function() {
+		    	var lastUrlVisitedOnThisTab = previousUrls["p" + openedTab.openerTabId.toString()] || "null";
+				addVisitToTree(openedTab.id, openedTab, openedTab, currentTabTree, lastUrlVisitedOnThisTab);
+		    });
+		});
+	}
+	isNewTab = false;
+});
 
 function onMessageListener_ (message, sender, sendResponse) {
 	if (message.type === 'getJSON') {
 		if (!currentTabTree) {
 			console.log('getting json for tab ' + String(activeTab));
-			chrome.storage.sync.get({[String(activeTab)]: []}, function (obj) {
-			    currentTabTree = obj[activeTab] || [];
-			    console.log(JSON.stringify({'result':currentTabTree}));
-			    sendResponse({'result':currentTabTree});
+			chrome.storage.sync.get({'tabMap': {}}, function (storage) {
+				var mappedTab = storage.tabMap[activeInfo.tabId]
+				chrome.storage.sync.get({[String(mappedTab)]: []}, function (obj) {
+				    currentTabTree = obj[activeTab] || [];
+				    console.log(JSON.stringify({'result':currentTabTree}));
+				    sendResponse({'result':currentTabTree});
+				});
 			});
 		} else {
 			console.log(JSON.stringify({'result':currentTabTree}));
@@ -141,3 +218,19 @@ function extractDomain_(url) {
 
     return domain;
 }
+
+/*
+		if (activeTab === openedTab.openerTabId) {
+			chrome.storage.sync.get({[String(openedTab.openerTabId)]: [],'previousUrls': {}}, function (storage) {
+				previousUrls = storage.previousUrls || {};
+				var parentTabTree = storage[String(openedTab.openerTabId)] || [];
+				var newTabTree = JSON.parse(JSON.stringify(parentTabTree));
+				var lastUrlVisitedOnThisTab = previousUrls["p" + openedTab.openerTabId.toString()] || "null";
+				addVisitToTree(openedTab.id, openedTab, openedTab, newTabTree, lastUrlVisitedOnThisTab);
+			});
+		} else {
+			var newTabTree = JSON.parse(JSON.stringify(currentTabTree));
+			var lastUrlVisitedOnThisTab = previousUrls["p" + openedTab.openerTabId.toString()] || "null";
+			addVisitToTree(openedTab.id, openedTab, openedTab, newTabTree, lastUrlVisitedOnThisTab);
+		}
+		*/
