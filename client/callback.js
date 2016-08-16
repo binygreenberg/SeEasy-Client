3e5bf9111cdd9a2806009c4c2c76a236bbb4a818
binyamin.greenberg <binyamin.greenberg@servicenow.com>
2016-08-16 23:23:58 +0300

    added radial button

--- client/callback.js
+++ client/callback.js
@@ -1,40 +1,3 @@
-// function showGraph(){
-// 	console.log('in showGraph method');
-// 	chrome.runtime.sendMessage(
-// 	    {'type': 'getJSON'},
-// 	    function generateList(response) {
-// 	    var data = response.result;
-
-// 	    // *********** Convert flat data into a nice tree ***************
-// 		// create a name:node map
-// 		console.log(data);
-// 		var dataMap = data.reduce(function(map, node) {
-// 			map[node.name] = node;
-// 			return map;
-// 		}, {});
-// 		console.log(dataMap);
-// 		// create the tree array
-// 		var treeData = [];
-// 		data.forEach(function(node) {
-// 			// add to parent
-// 			var parent = dataMap[node.parent];
-// 			if (parent) {
-// 				// create child array if it doesn't exist
-// 				(parent.children || (parent.children = []))
-// 					// add node to child array
-// 					.push(node);
-// 			} else {
-// 				// parent is null or missing
-// 				treeData.push(node);
-// 			}
-// 		});
-
-// 	    root = treeData[0];
-// 	    console.log("callback function root", root);
-// 	    setListRoot(root);
-// 	});
-// }
-
 function show(functionType){
 	console.log('in showTree method');
 	chrome.runtime.sendMessage(
@@ -72,7 +35,7 @@ function show(functionType){
 	});
 }
 
-//document.getElementById("graph").addEventListener("click", showGraph());
+show(setListRoot)
 
 // Add event listeners once the DOM has fully loaded by listening for the
 document.addEventListener('DOMContentLoaded', function () {
